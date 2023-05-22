bb11de0fa21afe222799ff31e274ad83a6e683ea
JATENCIA <atenciajaramilloj@gmail.com>
2023-05-22 14:31:10 -0500

    First commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+// import hero from "../assets/images/person.png";
+import hero from "../assets/logo.svg";
+const Hero = () => {
+  const social_media = [
+    {
+      icon: "logo-instagram",
+      link: "https://www.instagram.com/atenciajaramillo/",
+    },
+    { icon: "logo-github", link: "https://github.com/JATENCIA" },
+    {
+      icon: "logo-linkedin",
+      link: "https://www.linkedin.com/in/joseantonioatenciajaramillo/",
+    },
+    { icon: "logo-twitter", link: "https://twitter.com/joseantonioate4" },
+  ];
+  return (
+    <section
+      id="home"
+      className="min-h-screen flex py-10 md:flex-row flex-col items-center"
+    >
+      <div className="flex-1 flex items-center justify-center h-full">
+        <img
+          src={hero}
+          alt=""
+          className="md:w-4/6 h-full object-cover w-8/12"
+        />
+      </div>
+      <div className="flex-1">
+        <div className="md:text-left text-center">
+          <h1 className="md:text-5xl text-2xl md:leading-normal leading-10 text-white font-bold">
+            <span className="text-cyan-600 md:text-6xl text-5xl">
+              Hello!
+              <br />
+            </span>
+            We are <span>Arcome</span>
+          </h1>
+          <h4 className="md:text-2xl text-lg md:leading-normal leading-5 mt-4 font-bold text-gray-500">
+            web service
+          </h4>
+          <a href="#contact">
+            <button className="btn-primary mt-8">Contact Me</button>
+          </a>
+          <div className="mt-8 text-3xl flex items-center md:justify-start justify-center gap-5">
+            {social_media?.map((icon) => (
+              <div
+                key={icon.icon}
+                className="text-gray-600 hover:text-white cursor-pointer "
+              >
+                <a href={icon.link} target="_blank">
+                  <ion-icon name={icon.icon}></ion-icon>
+                </a>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
